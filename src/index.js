acf3ce5ff9fa5d5edf2f1a816de33b0066bf94c5
Vicente <vicente.barra.94@gmail.com>
2021-01-27 03:09:25 -0300

    1st commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// import { TodoList } from './classes/todo-list.class';
+// import { Todo } from './classes/todo.class';
+import './styles.css';
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
+
+export const todoList = new TodoList();
+
+// todoList.todos.forEach( todo => crearTodoHtml( todo ) );
+// Lo mismo -> / solo funciona con un argumento
+todoList.todos.forEach ( crearTodoHtml );
+
+console.log( 'todos', todoList.todos );
